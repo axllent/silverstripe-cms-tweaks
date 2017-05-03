51c9fff9551990eb46e752d156d2a0cd2bfa2d7c
axllent <axllent@gmail.com>
2017-05-03 17:06:26 +1200

    SilverStripe 4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SilverStripe 3 CMS Tweaks
+# SilverStripe 4 CMS Tweaks
 Module to add a series of tweaks/modifications to the SilverStripe CMS.
 The goal is to make the CMS less confusing for non-technical users, removing
 functionality some they can't use (ie: non SITETREE_REORGANISE users).
@@ -20,11 +20,8 @@ functionality some they can't use (ie: non SITETREE_REORGANISE users).
 - Remove "Duplicate" content menu when user has no `SITETREE_REORGANISE` permissions
 - Prevent accidental form submission in CMS when the enter key is used
 - Add "Remove formatting" button to TinyMCE to clean code
-- Add some invalid elements to TinyMCE
-- Optional "noenter" class can be added to any TextFields to prevent new lines (eg: MetaDescription)
 - Link logo in CMS to website homepage rather than silverstripe.org
 - Remove "Help" link
-- Add timestamps to TinyMCE's `content_css` css files to ensure changed css doesn't get cached
 
 ## Requirements
-- SilverStripe ~3.3
+- SilverStripe ^4.0
--- javascript/cms-tweaks.js
+++ javascript/cms-tweaks.js
@@ -1,12 +1,6 @@
 (function($){
 	$.entwine('ss', function($){
 
-		$('#tab-Root_Advanced').entwine({
-			onmatch: function(){
-				this.attr('title', 'Search engine optimization (SEO) etc');
-			}
-		});
-
 		/* Prevent enter key from submitting */
 		$('.cms-edit-form .field.date input, .cms-edit-form .field.text input,' +
 			'.cms-edit-form .noenter').entwine({
@@ -16,65 +10,48 @@
 			}
 		});
 
-		/* add auto-submit functionality */
-		$('#Form_ItemEditForm_AutoSubmitForm').entwine({
-			onmatch: function(){
-				$('#Form_ItemEditForm_action_doSave').trigger('click');
-				$('#Form_ItemEditForm_action_doSave').hide();
-				$('<p class="message good">Creating record, please wait...</p>' ).prependTo( '#Root_Main');
-				$('#Root_Main').find('div').hide();
-			}
-		});
-
-		/* hide the delete button */
-		$('#Form_ItemEditForm_HideDeleteButton').entwine({
-			onmatch: function(){
-				$('#Form_ItemEditForm_action_doDelete').hide();
-			}
-		});
-
 		/* Provide word / character stats on MetaDescription & MetaTitle input fields */
-		$('input#Form_EditForm_MetaTitle').entwine({
-			onkeyup: function(){
-				this.updateStats();
-			},
-			onmatch: function(){
-				this.updateStats();
-			},
-			updateStats: function(){
-				var wordCounts = {};
-				var v = this.val().trim();
-				var matches = v.match(/\b/g);
-				wordCounts[this.id] = matches ? matches.length / 2 : 0;
-				var words = 0;
-				$.each(wordCounts, function(k, v){
-					words += v;
-				});
-				var chars = v.replace(/\s+/g, ' ').length;
-				$('#' + this.attr('id') + 'Stats').text(words + ' words | ' + chars +' chars');
-			}
-		});
+		// $('input#Form_EditForm_MetaTitle').entwine({
+		// 	onkeyup: function(){
+		// 		this.updateStats();
+		// 	},
+		// 	onmatch: function(){
+		// 		this.updateStats();
+		// 	},
+		// 	updateStats: function(){
+		// 		var wordCounts = {};
+		// 		var v = this.val().trim();
+		// 		var matches = v.match(/\b/g);
+		// 		wordCounts[this.id] = matches ? matches.length / 2 : 0;
+		// 		var words = 0;
+		// 		$.each(wordCounts, function(k, v){
+		// 			words += v;
+		// 		});
+		// 		var chars = v.replace(/\s+/g, ' ').length;
+		// 		$('#' + this.attr('id') + 'Stats').text(words + ' words | ' + chars +' chars');
+		// 	}
+		// });
 
-		$('textarea#Form_EditForm_MetaDescription').entwine({
-			onkeyup: function(){
-				this.updateStats();
-			},
-			onmatch: function(){
-				this.updateStats();
-			},
-			updateStats: function(){
-				var wordCounts = {};
-				var v = this.val().trim();
-				var matches = v.match(/\b/g);
-				wordCounts[this.id] = matches ? matches.length / 2 : 0;
-				var words = 0;
-				$.each(wordCounts, function(k, v){
-					words += v;
-				});
-				var chars = v.replace(/\s+/g, ' ').length;
-				$('#' + this.attr('id') + 'Stats').text(words + ' words | ' + chars +' chars');
-			}
-		});
+		// $('textarea#Form_EditForm_MetaDescription').entwine({
+		// 	onkeyup: function(){
+		// 		this.updateStats();
+		// 	},
+		// 	onmatch: function(){
+		// 		this.updateStats();
+		// 	},
+		// 	updateStats: function(){
+		// 		var wordCounts = {};
+		// 		var v = this.val().trim();
+		// 		var matches = v.match(/\b/g);
+		// 		wordCounts[this.id] = matches ? matches.length / 2 : 0;
+		// 		var words = 0;
+		// 		$.each(wordCounts, function(k, v){
+		// 			words += v;
+		// 		});
+		// 		var chars = v.replace(/\s+/g, ' ').length;
+		// 		$('#' + this.attr('id') + 'Stats').text(words + ' words | ' + chars +' chars');
+		// 	}
+		// });
 
 		/* move any notices to above the tabs */
 		$('#Root .noticefield').entwine({
@@ -96,12 +73,12 @@
 	});
 
 	/* Set website logo to return to website */
-	$('div.cms-logo a').entwine({
-		onmatch: function(){
-			this.attr('href', '');
-			this.attr('title', 'Return to website');
-			this.removeAttr('target');
-		}
-	});
+	// $('div.cms-logo a').entwine({
+	// 	onmatch: function(){
+	// 		this.attr('href', '');
+	// 		this.attr('title', 'Return to website');
+	// 		this.removeAttr('target');
+	// 	}
+	// });
 
 })(jQuery);
