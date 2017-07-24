dc816082811666d6a3862e302112fe7c27686477
axllent <axllent@gmail.com>
2017-07-24 17:07:58 +1200

    Error pages

--- javascript/hide-error-pages.js
+++ javascript/hide-error-pages.js
@@ -4,9 +4,9 @@
  */
 (function($){
 	$.entwine('ss', function($){
-
+        // alert("yes");
 		/* Hide error pages in SiteTree */
-		$('li[data-pagetype="ErrorPage"]').entwine({
+		$('li[data-pagetype$="\ErrorPage"]').entwine({
 			onmatch: function(){
 				this.hide();
 			}
@@ -18,24 +18,6 @@
                 this.hide();
             }
         });
-
-		/* Hide error-xxx.html from file selector dropdowns */
-		$('li.class-File').entwine({
-			onmatch: function(){
-				var c = this.text().trim();
-				if (c.match(/^error-(\d+){3}\.html$/))
-					this.hide();
-			}
-		});
-
-		/* Hide error-xxx.html from File gridfields */
-		$('td.col-Title').entwine({
-			onmatch: function(){
-				var c = this.text().trim();
-				if (c.match(/^error-(\d+){3}\.html$/))
-					this.parent().hide();
-			}
-		});
 
 	});
 })(jQuery);
